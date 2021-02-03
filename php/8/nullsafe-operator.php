30c357e01cb88d62b14d9b15ff55fe26090f1bed
rizimore <rizimore@gmail.com>
2021-02-04 03:39:52 +0500

    php 8 nullsafe operator

--- php/8/nullsafe-operator.php
+++ php/8/nullsafe-operator.php
@@ -1,5 +1,20 @@
 <?php
 
 class User {
-
+    public function getMembership() {
+        return new Membership();
+    }
 }
+
+class Membership {
+    public function plan(): string
+    {
+        return "monthly";
+    }
+}
+
+$user = new User();
+$plan = $user->getMembership()->plan();
+$plan = $user->getMembership()?->plan();
+
+var_dump($plan);
